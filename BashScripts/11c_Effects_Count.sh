7cfb39b53a75345a67bfd5a2a7b4497d1e2913e5
Santiago José Ávila Quintero <sjaq@login1.hsn.dardel.pdc.kth.se>
2025-06-01 22:38:05 +0200

    Variant effect count updated - Depth stats corrected

---
+++ BashScripts/11c_Effects_Count.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Count the number of sites based on the impact category
+
+mamba activate snpeff #Activate before running 
+
+zcat /cfs/klemming/projects/snic/snic2022-23-124/Santiago/Purging/All_Origins_annotated.vcf.gz | \
+SnpSift extractFields -e "." - CHROM POS ANN[0].IMPACT | \
+sort -u | \
+cut -f 3 | \
+sort | \
+uniq -c | \
+awk '{print $2 "\t" $1}' > /cfs/klemming/projects/snic/snic2022-23-124/Santiago/Purging/Effect_Counts.tsv
